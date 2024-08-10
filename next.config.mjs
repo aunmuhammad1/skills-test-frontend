e18580c5b1225a127e0a44d535b79b44a6c943cb
aunmuhammad1 <f20bseen1m01094@iub.edu.pk>
2024-08-10 16:24:19 +0500

    api

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: false, // Set to false to disable strict mode
-  };
-  
+  reactStrictMode: false,
+  env: {
+    NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL,
+  },
+};
+
 export default nextConfig;
-  
